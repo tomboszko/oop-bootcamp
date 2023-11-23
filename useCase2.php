fe5b67568daf92840c934c446b439602eb9ce3c2
Thomas Gomree <gomreethomas@gmail.com>
2023-11-23 09:11:27 +0100

    update use case 2

--- useCase2.php
+++ useCase2.php
@@ -6,39 +6,6 @@ and re-use it efficiently for every fruit? -->
 
 <?php
 
-$bananas_Q = 6;
-$bananas_P = 1;
-$apples_Q = 3;
-$apples_P = 1.5;
-$wine_Q = 2;
-$wine_P= 10;
-
-// Calculate total price
-$bananas_total = $bananas_Q* $bananas_P;
-$apples_total = $apples_Q* $apples_P;
-$wine_total = $wine_Q * $wine_P;
-
-// Calculate final total price
-$total_price = $bananas_total + $apples_total + $wine_total;
-
-echo "Total price: " . $total_price . " $" . "<br>";
-
-// tax rates
-$fruit_tax_rate = 0.06;
-$wine_tax_rate = 0.21;
-
-// tax for each item
-$bananas_tax = $bananas_total * $fruit_tax_rate;
-$apples_tax = $apples_total * $fruit_tax_rate;
-$wine_tax = $wine_total * $wine_tax_rate;
-
-// Calculate total tax
-$total_tax = $bananas_tax + $apples_tax + $wine_tax;
-
-echo "Total tax: " . $total_tax . " $" . "<br>";
-echo "Total price with tax: " . ($total_price + $total_tax) . " $" . "<br>";
-echo "with classes: <br>";
-
 // class
 class Basket {
     public $quantity;
